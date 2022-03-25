ec42d640e7f3de65aa9542a68d3095303f3d06c2
Christian Huggins <christianhuggins@Christians-MacBook-Pro.local>
2022-03-24 21:28:56 -0500

    Initial commit

---
+++ CircuitTester/app/src/main/java/com/example/circuittester/PhotoChoice.kt
@@ -0,0 +1,14 @@
+package com.example.circuittester
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Pop : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.photo_choice)
+    }
+
+}
